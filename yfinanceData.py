c92eb3f4a19c9f1bfb39a87b952aeed7d44f7a56
jacky.chen <jcsmile@gmail.com>
2025-01-22 13:59:13 -0600

    Add analyzers and print them

--- yfinanceData.py
+++ yfinanceData.py
@@ -3,6 +3,7 @@ import pandas as pd
 import backtrader as bt
 from datetime import datetime
 import osgf_strategy as osgf
+from sma_strategy import SMAStrategy
 
 import logging
 # Create and configure logger
@@ -67,40 +68,25 @@ class CustomPandasData(bt.feeds.PandasData):
     )
 
 # Step 4: Backtest Strategy
-class SMAStrategy(bt.Strategy):
-    def __init__(self):
-        self.sma20 = bt.indicators.SimpleMovingAverage(period=20)  # 20-day SMA
-        self.sma5 = bt.indicators.SimpleMovingAverage(period=5)  # 5-day SMA
+#from osgf_strategy import OsgfStrategy
+#from sma_strategy import SMAStrategy
 
-    def next(self):
-        dt = self.datas[0].datetime.date(0)
-        current_close = self.data.close[0]
-        previous_close = self.data.close[-1]  # Previous day's close price
+# Step 5: Add analyzer
+def add_analyzers(cerebro):
+    # 添加分析指标
+    # 返回年初至年末的年度收益率
+    cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name='_AnnualReturn')
+    # 计算最大回撤相关指标
+    cerebro.addanalyzer(bt.analyzers.DrawDown, _name='_DrawDown')
+    # 计算年化收益：日度收益
+    cerebro.addanalyzer(bt.analyzers.Returns, _name='_Returns', tann=252)
+    # 计算年化夏普比率：日度收益
+    cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='_SharpeRatio', timeframe=bt.TimeFrame.Days, annualize=True, riskfreerate=0) # 计算夏普比率
+    cerebro.addanalyzer(bt.analyzers.SharpeRatio_A, _name='_SharpeRatio_A')
+    # 返回收益率时序
+    cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='_TimeReturn')
 
-        current_sma5 = self.sma5[0]
-        previous_sma5 = self.sma5[-1]  # Previous day's sma5
-
-        current_sma20 = self.sma20[0]
-        previous_sma20 = self.sma20[-1]  # Previous day's sma20
-
-        # Check for "Price up cross SMA5" (close price crosses above 5-day SMA) during an uptrend
-        if previous_close <= previous_sma5 and current_close >= current_sma5 and current_sma5 > current_sma20 :
-            self.buy(size=10)
-            logger.info(f"{dt}, Price up cross Buy at {self.data.close[0]}, 10 shares")
-        # Check for "Golden Cross" (5-day SMA crosses above 20-day SMA) 
-        elif previous_sma5 < previous_sma20 and current_sma5 >= current_sma20:
-            self.buy(size=10)
-            logger.info(f"{dt}, Golden Cross Buy at {self.data.close[0]}, 10 shares")     
-        # Check for "Death Cross" (5-day SMA crosses below 20-day SMA)
-        elif previous_sma5 > previous_sma20 and current_sma5 <= current_sma20:
-            self.sell(size=10)
-            logger.info(f"{dt}, Death Cross Sell at {self.data.close[0]}, 10 shares")
-        # Check for "Price down cross SMA5" (close price crosses below 5-day SMA) during a downtrend
-        elif previous_close > previous_sma5 and current_close <= current_sma5 and current_sma5 < current_sma20:
-            self.sell(size=10)
-            logger.info(f"{dt}, Price down cross Sell at {self.data.close[0]}, 10 shares")
-
-# Step 5: Run the backtest
+# Step 6: Run the backtest
 def run_backtest(filename="nvda_data.csv"):
     # Download and save data
     ticker = "NVDA"
@@ -119,15 +105,42 @@ def run_backtest(filename="nvda_data.csv"):
     cerebro.broker.set_cash(100000.0)  # Initial cash
     cerebro.broker.setcommission(commission=0.001)  # Commission for trades
 
+    add_analyzers(cerebro)  # Add analyzers
     # Print starting cash
     print(f"Starting Portfolio Value: ${cerebro.broker.getvalue():,.2f}")
 
     # Run backtest
-    cerebro.run()
+    result = cerebro.run()
 
     # Print final cash
     print(f"Final Portfolio Value: ${cerebro.broker.getvalue():,.2f}")
 
+    # Print Analyzers
+    print("--------------- AnnualReturn -----------------")
+    print(result[0].analyzers._AnnualReturn.get_analysis())
+    print("--------------- DrawDown -----------------")
+    print(result[0].analyzers._DrawDown.get_analysis())
+    print("--------------- Returns -----------------")
+    print(result[0].analyzers._Returns.get_analysis())
+    print("--------------- SharpeRatio -----------------")
+    print(result[0].analyzers._SharpeRatio.get_analysis())
+    print("--------------- SharpeRatio_A -----------------")
+    print(result[0].analyzers._SharpeRatio_A.get_analysis())
+
+    # 常用指标提取
+    analyzer = {}
+    # 提取年化收益
+    analyzer['年化收益率'] = result[0].analyzers._Returns.get_analysis()['rnorm']
+    analyzer['年化收益率（%）'] = result[0].analyzers._Returns.get_analysis()['rnorm100']
+    # 提取最大回撤
+    analyzer['最大回撤（%）'] = result[0].analyzers._DrawDown.get_analysis()['max']['drawdown'] * (-1)
+    # 提取夏普比率
+    analyzer['年化夏普比率'] = result[0].analyzers._SharpeRatio_A.get_analysis()['sharperatio']
+    print("--------------- analyzers -----------------")
+    print(analyzer)
+
+    # Daily return series
+    ret = pd.Series(result[0].analyzers._TimeReturn.get_analysis())
     # Plot the results
     cerebro.plot()
 
