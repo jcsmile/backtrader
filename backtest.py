460c3983e6c56a922a18b6e55e1dd3cd713a554f
jacky.chen <jcsmile@gmail.com>
2025-03-19 12:38:34 -0500

    Add the backtest class

--- backtest.py
+++ backtest.py
@@ -5,15 +5,20 @@ from datetime import datetime
 import strategies.osgf_strategy as osgf
 from strategies.sma_strategy import SMAStrategy
 from strategies.volume_strategy import VolumeStrategy
+from strategies.custom_strategy import CustomStrategy
 from analyzers.key_Indicator_analyzer import KeyIndicatorAnalyzer
 from analyzers.trade_list_analyzer import TradeListAnalyzer
 from dotenv import load_dotenv
 import os
 import logging
+from data.source_data import SourceData
 from data.yfinanceData import YFinanceData
 from data.tradingviewData import TradingViewData
 from data.custom_pandas_data import CustomPandasData
+import matplotlib
+import matplotlib.pyplot as plt
 
+matplotlib.use('TkAgg') 
 # Create and configure logger
 logging.basicConfig(level=logging.INFO,
                     format='%(message)s'
@@ -108,8 +113,8 @@ def run_backtest():
     # Create Cerebro instance
     cerebro = bt.Cerebro()
     # Add the test strategy
-    #cerebro.addstrategy(SMAStrategy, short_period=5, long_period=10)
-    cerebro.addstrategy(VolumeStrategy) 
+    cerebro.addstrategy(SMAStrategy, short_period=5, long_period=10)
+    #cerebro.addstrategy(VolumeStrategy) 
     # add optimizer of strategy
     #cerebro.optstrategy(SMAStrategy, short_period=range(3, 6, 1), long_period=range(10, 21, 10))
 
@@ -140,5 +145,83 @@ def run_backtest():
     # Plot the results
     cerebro.plot()
 
+STRATEGY_CLASSES = {
+    "SMAStrategy": SMAStrategy,
+    "VolumeStrategy": VolumeStrategy,
+    "CustomStrategy": CustomStrategy,
+}
+
+class Backtest:
+    def __init__(self, source_data, cash=100000, commission=1 / 1000, slip_type=-1, slip_value=0, IS_ALL_IN=False,
+                 params_dict={}, make_plot=True):
+        self.source_data = source_data
+        self.cash = cash
+        self.commission = commission
+        self.slip_type = slip_type
+        self.slip_value = slip_value
+        self.IS_ALL_IN = IS_ALL_IN
+        self.params_dict = params_dict
+        self.make_plot = make_plot
+        self.origin_cash = 0
+        self.cls_ret = {}   
+
+    def fetch_data(self, ticker, exchange):
+        daily_price_data = self.source_data.refresh_data(ticker, exchange)
+        data_feed = CustomPandasData(dataname=daily_price_data)
+        return data_feed
+    
+    def run(self, ticker, exchange):
+        strategy_params = self.params_dict.get("strategy_params", {})
+        analyzer_params = self.params_dict.get('analyzers', {})
+        plot_params = self.params_dict.get("plot", {})
+        strategy_class_name = self.params_dict.get('strategy_class', "SMAStrategy")  # Default to SMAStrategy if not provided
+        strategy_class = STRATEGY_CLASSES.get(strategy_class_name)  # Retrieve the class reference
+        if strategy_class is None:
+            raise ValueError(f"Invalid strategy class name: {strategy_class_name}")
+                
+        datas = self.fetch_data(ticker, exchange)
+        cerebro = bt.Cerebro(cheat_on_open=self.IS_ALL_IN)
+        cerebro.addstrategy(strategy_class, **strategy_params)
+        #cerebro.addstrategy(SMAStrategy, short_period=5, long_period=10)
+
+        if isinstance(datas, list):
+            for item in datas:
+                cerebro.adddata(item)
+        else:
+            cerebro.adddata(datas, name=ticker)
+
+        cerebro.broker.setcash(self.cash)
+        cerebro.broker.setcommission(self.commission)
+
+        if self.slip_type == 0:
+            cerebro.broker.set_slippage_fixed(self.slip_value)
+        elif self.slip_type == 1:
+            cerebro.broker.set_slippage_perc(self.slip_value)
+
+        for ana_name, ana_class in analyzer_params.items():
+            cerebro.addanalyzer(ana_class, _name=ana_name)
+
+        back_ret = cerebro.run()
+
+        if self.make_plot:
+            cerebro.plot(**plot_params) 
+            plt.show()  # 添加这一行以确保图像窗口保持打开状态
+        return back_ret, cerebro
+    
 if __name__ == "__main__":
-    run_backtest()
+    #run_backtest()
+
+
+
+    tv_data = TradingViewData()
+    tester = Backtest(tv_data, cash=100000, commission=1 / 1000, slip_type=-1, slip_value=0, IS_ALL_IN=False,
+                    params_dict={"strategy_params": {"short_period": 5, "long_period": 10},
+                                 "strategy_class": "SMAStrategy",
+                                    'analyzers': {'sharp': bt.analyzers.SharpeRatio,
+                                                    'annual_return': bt.analyzers.AnnualReturn,
+                                                    'drawdown': bt.analyzers.DrawDown}})
+    ret, cere = tester.run("AAPL", "NASDAQ")
+    print('Sharpe Ratio: ', ret[0].analyzers.sharp.get_analysis())
+    print('drawdown: ', ret[0].analyzers.drawdown.get_analysis())
+    #cere.plot(style='candle')
+    input("Press Enter to continue...")
--- indicators/volume_indicator.py
+++ indicators/volume_indicator.py
@@ -1,30 +1,43 @@
+import backtrader as bt
+import numpy as np
+#import matplotlib.pyplot as plt
+
 class VolumeIndicator(bt.Indicator):
     lines = ('volume_label',)
-    plotinfo = dict(subplot=False)
+    plotinfo = dict(subplot=True)
 
-    def __init__(self, period=20):
-        self.period = period
+    def __init__(self):
         self.volume_data = []
+        self.addminperiod(252 // 2)
 
     def next(self):
         # Append the current volume to the volume data list
-        self.volume_data.append(self.data.volume[0])
+        self.volume_data.append(self.data.volume[0] if self.data.volume[0] > 0 else 1)
 
-        # Keep only the last 'period' volumes for analysis
-        if len(self.volume_data) > self.period:
-            self.volume_data.pop(0)
+        # Calculate mean and standard deviation of the historical volumes in log scale
+        log_volumes = np.log(self.volume_data)
+        mean_log_volume = np.mean(log_volumes)
+        std_log_volume = np.std(log_volumes)
 
-        # Calculate mean and standard deviation of the historical volumes
-        if len(self.volume_data) == self.period:
-            mean_volume = sum(self.volume_data) / self.period
-            std_volume = (sum((x - mean_volume) ** 2 for x in self.volume_data) / self.period) ** 0.5
-
-            # Determine if the current volume is high or low
-            if self.data.volume[0] > mean_volume + std_volume:
-                self.lines.volume_label[0] = "high vol"
-            elif self.data.volume[0] < mean_volume - std_volume:
-                self.lines.volume_label[0] = "low vol"
-            else:
-                self.lines.volume_label[0] = "normal vol"
+        # Determine if the current volume is high or low based on log-normal distribution
+        current_log_volume = np.log(self.data.volume[0])
+        if current_log_volume > mean_log_volume + std_log_volume:
+            self.lines.volume_label[0] = 1  # "hi"
+        elif current_log_volume < mean_log_volume - std_log_volume:
+            self.lines.volume_label[0] = -1  # "lo"
         else:
-            self.lines.volume_label[0] = "loading"  # Not enough data yet
+            self.lines.volume_label[0] = 0  # "nv"
+"""
+    def plot(self):
+        # Custom plot method to display labels
+        fig, ax = plt.subplots()
+        ax.plot(self.data.datetime.array, self.data.volume.array, label='Volume')
+        for i, label in enumerate(self.lines.volume_label):
+            if label == 1:
+                ax.text(self.data.datetime.array[i], self.data.volume.array[i], 'hi', color='red' if self.data.close[i] < self.data.open[i] else 'green')
+            elif label == -1:
+                ax.text(self.data.datetime.array[i], self.data.volume.array[i], 'lo', color='red' if self.data.close[i] < self.data.open[i] else 'green')
+            elif label == 0:
+                ax.text(self.data.datetime.array[i], self.data.volume.array[i], 'nv', color='red' if self.data.close[i] < self.data.open[i] else 'green')
+        plt.show()
+        """
